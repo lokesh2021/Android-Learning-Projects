86b62612f75082ddbf52a707094bc8414a4f1402
lokesh2021 <lokeshbandi2021@gmail.com>
2020-11-29 17:33:57 +0530

    Layouts Commit

---
+++ layouts/app/src/main/java/com/lokesh/layouts/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.lokesh.layouts
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.lokesh.layouts.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        findViewById<Button>(R.id.done).setOnClickListener {
+            addNickname(it)
+        }
+
+    }
+
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        //To Hide the Keyboard
+        val inm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inm.hideSoftInputFromWindow(view.windowToken,0)
+
+    }
+}
---
+++ layouts/app/src/main/java/com/lokesh/layouts/MyName.kt
@@ -0,0 +1,3 @@
+package com.lokesh.layouts
+
+data class MyName(var name:String = "", var nickname:String = "")
