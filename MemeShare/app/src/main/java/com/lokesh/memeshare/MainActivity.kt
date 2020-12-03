eeba9ad8aff45d8449185de2f779f88a640d63ab
lokesh2021 <lokeshbandi2021@gmail.com>
2020-12-03 15:32:56 +0530

    meme project

---
+++ MemeShare/app/src/main/java/com/lokesh/memeshare/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.lokesh.memeshare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun NextButton(view: View) {}
+    fun ShareButton(view: View) {}
+}
