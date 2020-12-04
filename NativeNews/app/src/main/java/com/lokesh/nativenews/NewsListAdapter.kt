6fdafdc8823ffba1a7eab80af00ad053e86f759d
lokesh2021 <lokeshbandi2021@gmail.com>
2020-12-04 13:18:28 +0530

    update

---
+++ NativeNews/app/src/main/java/com/lokesh/nativenews/NewsListAdapter.kt
@@ -0,0 +1,13 @@
+package com.lokesh.nativenews
+
+import android.view.View
+import android.widget.AbsListView
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsListAdapter:RecyclerView.Adapter<>{
+
+}
+
+class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
