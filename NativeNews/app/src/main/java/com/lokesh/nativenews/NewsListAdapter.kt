77eea79507dad06bac87239bef39a04cd3add99d
lokesh2021 <lokeshbandi2021@gmail.com>
2020-12-04 13:23:13 +0530

    Update NewsListAdapter.kt

--- NativeNews/app/src/main/java/com/lokesh/nativenews/NewsListAdapter.kt
+++ NativeNews/app/src/main/java/com/lokesh/nativenews/NewsListAdapter.kt
@@ -7,13 +7,13 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import org.w3c.dom.Text
 
-class NewsListAdapter: RecyclerView.Adapter<NewsViewHolder>() {
+class NewsListAdapter(val items: ArrayList<String>) : RecyclerView.Adapter<NewsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
         TODO("Not yet implemented")
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return items.size
     }
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
